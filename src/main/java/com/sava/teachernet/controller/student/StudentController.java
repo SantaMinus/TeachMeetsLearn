9947a092e82108fffb7a5b10b9c9aa55a399ac6d
SantaMinus <santaminus@gmail.com>
2023-12-25 20:39:33 +0200

    Add controller method for profile; add getStudents()

--- src/main/java/com/sava/teachernet/controller/student/StudentController.java
+++ src/main/java/com/sava/teachernet/controller/student/StudentController.java
@@ -1,14 +1,36 @@
 package com.sava.teachernet.controller.student;
 
+import com.sava.teachernet.model.Student;
+import com.sava.teachernet.service.StudentService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/student")
+@RequiredArgsConstructor
 public class StudentController {
+  private StudentService studentService;
 
-  @GetMapping("/student/dashboard")
+  @GetMapping("/dashboard")
   public String showStudentDashboard() {
     return "student/dashboard";
+  }
+
+  @GetMapping("/profile")
+  public String showStudentProfile(Model model) {
+    Student student = Student.builder().build();
+
+    model.addAttribute(student);
+    return "student/profile";
+  }
+
+  @GetMapping
+  public List<Student> getStudents() {
+    return studentService.getAll();
   }
 }
 
