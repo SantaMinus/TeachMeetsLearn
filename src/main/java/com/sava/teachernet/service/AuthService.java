1dda7898253da61458bc19a37a36e4781ef44ba0
santaminus <santaminus@gmail.com>
2024-06-30 19:12:08 +0300

    [6] - Add AuthService

---
+++ src/main/java/com/sava/teachernet/service/AuthService.java
@@ -0,0 +1,33 @@
+package com.sava.teachernet.service;
+
+import com.sava.teachernet.dto.SignUpDto;
+import com.sava.teachernet.exception.InvalidJwtException;
+import com.sava.teachernet.model.UserEntity;
+import com.sava.teachernet.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+  @Autowired
+  UserRepository repository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) {
+    var user = repository.findByLogin(username);
+    return user;
+  }
+
+  public UserDetails signUp(SignUpDto data) throws InvalidJwtException {
+    if (repository.findByLogin(data.login()) != null) {
+      throw new InvalidJwtException("Username already exists");
+    }
+    String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+    UserEntity newUser = new UserEntity(data.login(), encryptedPassword, data.role());
+    return repository.save(newUser);
+  }
+}
