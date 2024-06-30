64cd04e6155db14a70331508dc9e835c61f3e60c
santaminus <santaminus@gmail.com>
2024-06-30 19:10:34 +0300

    [6] - Add token provider

--- src/main/java/com/sava/teachernet/TeacherNetApplication.java
+++ src/main/java/com/sava/teachernet/TeacherNetApplication.java
@@ -2,8 +2,10 @@ package com.sava.teachernet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:/Users/sava/.env")
 public class TeacherNetApplication {
 
 	public static void main(String[] args) {
