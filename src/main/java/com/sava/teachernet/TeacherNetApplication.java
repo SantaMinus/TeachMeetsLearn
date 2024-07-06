fc5ad392b461ccb1f061e00a4fa2251afb656787
santaminus <santaminus@gmail.com>
2024-07-06 19:42:27 +0300

    [6] - Remove @PropertySource

--- src/main/java/com/sava/teachernet/TeacherNetApplication.java
+++ src/main/java/com/sava/teachernet/TeacherNetApplication.java
@@ -2,10 +2,8 @@ package com.sava.teachernet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("file:/Users/sava/.env")
 public class TeacherNetApplication {
 
 	public static void main(String[] args) {
