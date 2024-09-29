74b2377f9a10c0c59f432fab583be23081a9e3f2
santaminus <santaminus@gmail.com>
2024-09-29 21:36:23 +0300

    [TML-8] - Cover TeacherService with tests

--- src/test/java/com/sava/teachernet/util/TestDataFactory.java
+++ src/test/java/com/sava/teachernet/util/TestDataFactory.java
@@ -5,12 +5,23 @@ import static com.sava.teachernet.util.Constants.TEST_USER_LAST_NAME;
 import static com.sava.teachernet.util.Constants.TEST_USER_NAME;
 
 import com.sava.teachernet.model.Student;
+import com.sava.teachernet.model.Teacher;
 import com.sava.teachernet.model.User;
 
 public class TestDataFactory {
 
   public static Student buildTestStudent() {
     return Student.builder()
+        .name(TEST_USER_NAME)
+        .lastName(TEST_USER_LAST_NAME)
+        .user(User.builder()
+            .login(TEST_LOGIN)
+            .build())
+        .build();
+  }
+
+  public static Teacher buildTestTeacher() {
+    return Teacher.builder()
         .name(TEST_USER_NAME)
         .lastName(TEST_USER_LAST_NAME)
         .user(User.builder()
