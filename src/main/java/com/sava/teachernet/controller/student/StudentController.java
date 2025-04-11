ec69e9823ef2288762747c8c742f1202b2b42b17
santaminus <santaminus@gmail.com>
2025-04-11 18:05:55 +0300

    [TML-44] - Remove Teacher from Student

--- src/main/java/com/sava/teachernet/controller/student/StudentController.java
+++ src/main/java/com/sava/teachernet/controller/student/StudentController.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,6 +53,12 @@ public class StudentController {
   @PostMapping("/me/teachers")
   public String assignTeacherToCurrentStudent(@RequestParam Long teacherId) {
     studentService.assignTeacherToCurrentStudent(teacherId);
+    return "redirect:/students/me/teachers";
+  }
+
+  @DeleteMapping("/me/teachers")
+  public String unassignTeacherFromCurrentStudent(@RequestParam Long teacherId) {
+    studentService.unassignTeacherFromCurrentStudent(teacherId);
     return "redirect:/students/me/teachers";
   }
 }
