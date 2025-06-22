fd491ab982a0a12cfbaee2044f1ef1d853e93ca0
santaminus <santaminus@gmail.com>
2025-06-22 18:43:27 +0300

    [TML-43] - Fix input params

--- src/main/java/com/sava/teachernet/dto/SearchDto.java
+++ src/main/java/com/sava/teachernet/dto/SearchDto.java
@@ -1,15 +1,14 @@
 package com.sava.teachernet.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
-@Getter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SearchDto {
+
   private String name;
   private String lastName;
   private String subject;
