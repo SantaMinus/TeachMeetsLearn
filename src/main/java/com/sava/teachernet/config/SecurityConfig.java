76c0c05340a86aba530462dc5f6b9c2ac94926c4
santaminus <santaminus@gmail.com>
2025-07-20 18:20:33 +0300

    [TML-47] - Add GitHub login option

--- src/main/java/com/sava/teachernet/config/SecurityConfig.java
+++ src/main/java/com/sava/teachernet/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.sava.teachernet.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +11,10 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,21 +23,43 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
   @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    return httpSecurity
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
         .csrf(AbstractHttpConfigurer::disable)
-        .sessionManagement(
-            session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
-        .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/auth/**").permitAll()
-            .requestMatchers("/students").permitAll()
-            .requestMatchers("/teachers").permitAll()
-            .requestMatchers("/teachers/search").permitAll()
-            .requestMatchers("/students/**").hasRole("STUDENT")
-            .requestMatchers("/teachers/**").hasRole("TEACHER")
-            .anyRequest().authenticated())
-        .formLogin(withDefaults())
-        .build();
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+        .authorizeHttpRequests(auth ->
+            auth
+                .requestMatchers("/auth/**", "/login", "/oauth2/**", "/webjars/**",
+                    "/css/**", "/js/**").permitAll()
+                .requestMatchers("/students").permitAll()
+                .requestMatchers("/teachers").permitAll()
+                .requestMatchers("/teachers/search").permitAll()
+                .requestMatchers("/students/**").hasRole("STUDENT")
+                .requestMatchers("/teachers/**").hasRole("TEACHER")
+                .anyRequest().authenticated())
+        .formLogin(form -> form
+            .loginPage("/auth/login")
+            .loginProcessingUrl("/auth/login")
+            .usernameParameter("login")
+            .passwordParameter("password")
+            .defaultSuccessUrl("/", true)
+            .permitAll())
+        .oauth2Login(oauth2 -> oauth2
+            .loginPage("/auth/login")
+            .defaultSuccessUrl("/", true)
+            .userInfoEndpoint(userInfo -> userInfo
+                .userService(oauth2UserService())))
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/auth/login?logout")
+            .permitAll());
+    return http.build();
+  }
+
+  @Bean
+  public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+    return new DefaultOAuth2UserService();
   }
 
   @Bean
