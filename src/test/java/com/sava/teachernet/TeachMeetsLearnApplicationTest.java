2c0255638598f1c4c85ab6da4a38f85b3d043509
santaminus <santaminus@gmail.com>
2025-08-02 17:54:31 +0300

    [TML-47] - Fix tests

--- src/test/java/com/sava/teachernet/TeachMeetsLearnApplicationTest.java
+++ src/test/java/com/sava/teachernet/TeachMeetsLearnApplicationTest.java
@@ -2,9 +2,13 @@ package com.sava.teachernet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class TeachMeetsLearnApplicationTest {
+	@MockitoBean
+	private ClientRegistrationRepository clientRegistrationRepository;
 
 	@Test
 	void contextLoads() {
