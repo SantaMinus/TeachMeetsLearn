cc87784a2a525f250c70e36b4b40a769c1aad5de
santaminus <santaminus@gmail.com>
2026-06-23 18:43:29 +0300

    [TML-47] - Remove unused AuthController.signIn()

--- src/main/java/com/sava/teachernet/controller/AuthController.java
+++ src/main/java/com/sava/teachernet/controller/AuthController.java
@@ -1,12 +1,8 @@
 package com.sava.teachernet.controller;
 
-import com.sava.teachernet.dto.SignInDto;
 import com.sava.teachernet.dto.SignUpDto;
 import com.sava.teachernet.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class AuthController {
 
-  private final AuthenticationManager authenticationManager;
   private final AuthService service;
 
   @GetMapping("/signup")
@@ -34,12 +29,5 @@ public class AuthController {
   @GetMapping("/login")
   public String signInForm() {
     return "login";
-  }
-
-  @PostMapping("/login")
-  public ResponseEntity<Void> signIn(SignInDto data) {
-    var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-    authenticationManager.authenticate(usernamePassword);
-    return ResponseEntity.ok().build();
   }
 }
